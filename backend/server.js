11e92b0e87d1f0fc57a3e601de19406c683b45fe
Daniel Iheonu <64360587+danieliheonu@users.noreply.github.com>
2022-06-25 23:33:33 +0100

    rendered the frontend from the backend

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const dotenv = require('dotenv');
 const { mongoose } = require('mongoose');
 const cors = require('cors');
+const path = require('path')
 
 dotenv.config()
 
@@ -13,7 +14,13 @@ app.use(express.json())
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
 
 app.use(cors())
-app.use('/api/v1/transactions/', require('./routes/transactions'))
+app.use('/api/v1/transactions', require('./routes/transactions'))
+
+// if(process.env.NODE_ENV === 'production'){
+app.use(express.static('../frontend/build'));
+
+app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, '../frontend', 'build', 'index.html')))
+// }
 
 app.listen(process.env.PORT, () => {
     try {
